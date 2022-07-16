3a022c4489b8a02f433a6c3ba60cc535c29da9b3
Karim Mohammadi <s.karim.mohammadi@gmail.com>
2022-07-16 19:02:36 +0430

    format as WP-coding standards

--- includes/class-archive.php
+++ includes/class-archive.php
@@ -3,20 +3,22 @@
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'WP_List_Table' ) ) {
-	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 }
 
 class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 
+
 	public static $table = 'woocommerce_ir_sms_archive';
 
 	public function __construct() {
-
-		parent::__construct( [
-			'singular' => 'آرشیو پیامک های ووکامرس',
-			'plural'   => 'آرشیو پیامک های ووکامرس',
-			'ajax'     => false,
-		] );
+		parent::__construct(
+			array(
+				'singular' => 'آرشیو پیامک های ووکامرس',
+				'plural'   => 'آرشیو پیامک های ووکامرس',
+				'ajax'     => false,
+			)
+		);
 	}
 
 	public function no_items() {
@@ -33,7 +35,6 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 			case 'reciever':
 				return '<div style="direction:ltr !important;text-align:' . $align . ';">' . $item[ $column_name ] . '</div>';
 			default:
-
 				if ( is_string( $item[ $column_name ] ) ) {
 					return nl2br( $item[ $column_name ] );
 				}
@@ -44,12 +45,12 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 
 	public function column_cb( $item ): string {
 		return sprintf(
-			'<input type="checkbox" name="item[]" value="%s" />', $item['id']
+			'<input type="checkbox" name="item[]" value="%s" />',
+			$item['id']
 		);
 	}
 
 	public function column_post_id( $item ) {
-
 		if ( empty( $item['post_id'] ) ) {
 			return '-';
 		}
@@ -57,7 +58,7 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 		$post_id   = intval( $item['post_id'] );
 		$post_type = get_post_type( $post_id );
 
-		$value = [];
+		$value = array();
 
 		switch ( $post_type ) {
 
@@ -78,17 +79,16 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 				return '-';
 		}
 
-		$actions = [
+		$actions = array(
 			'delete' => sprintf( '<a target="_blank" href="%s">%s</a>', get_edit_post_link( $post_id ), $edit_title ),
-		];
+		);
 
-		$post_id = '<a title="' . $filter_title . '" href="' . add_query_arg( [ 'id' => $post_id ] ) . '">' . implode( ' :: ', $value ) . '</a>';
+		$post_id = '<a title="' . $filter_title . '" href="' . add_query_arg( array( 'id' => $post_id ) ) . '">' . implode( ' :: ', $value ) . '</a>';
 
 		return sprintf( '%1$s %2$s', $post_id, $this->row_actions( $actions ) );
 	}
 
 	public function column_result( $item ) {
-
 		$result = ! empty( $item['result'] ) ? $item['result'] : '';
 
 		if ( trim( $result ) == '_ok_' ) {
@@ -99,7 +99,6 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 	}
 
 	public function column_type( $item ) {
-
 		if ( empty( $item['type'] ) ) {
 			return '-';
 		}
@@ -182,15 +181,17 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 
 		$delete_nonce = wp_create_nonce( 'pwoosms_delete_archive' );
 
-		$url = add_query_arg( [
-			'action'   => 'delete',
-			'item'     => absint( $item['id'] ),
-			'_wpnonce' => $delete_nonce,
-		] );
+		$url = add_query_arg(
+			array(
+				'action'   => 'delete',
+				'item'     => absint( $item['id'] ),
+				'_wpnonce' => $delete_nonce,
+			)
+		);
 
-		$actions = [
-			'delete' => sprintf( '<a href="%s">حذف</a>', $url ),// @todo escape url
-		];
+		$actions = array(
+			'delete' => sprintf( '<a href="%s">حذف</a>', $url ), // @todo escape url
+		);
 
 		$date = date_i18n( 'Y-m-d H:i:s', strtotime( $item['date'] ) );
 		$date = PWooSMS()->mayBeJalaliDate( $date );
@@ -199,22 +200,23 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 	}
 
 	public function prepare_items() {
-
-		$this->_column_headers = [ $this->get_columns(), [], $this->get_sortable_columns() ];
+		$this->_column_headers = array( $this->get_columns(), array(), $this->get_sortable_columns() );
 		$this->process_bulk_action();
 
 		$per_page = 20;
 
-		$this->set_pagination_args( [
-			'total_items' => $this->record_count(),
-			'per_page'    => $per_page,
-		] );
+		$this->set_pagination_args(
+			array(
+				'total_items' => $this->record_count(),
+				'per_page'    => $per_page,
+			)
+		);
 
 		$this->items = $this->get_items( $per_page, $this->get_pagenum() );
 	}
 
 	public function get_columns(): array {
-		return [
+		return array(
 			'cb'       => '<input type="checkbox" />',
 			'date'     => 'زمان',
 			'post_id'  => 'سفارش / محصول',
@@ -223,21 +225,20 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 			'reciever' => 'گیرندگان',
 			'sender'   => 'وبسرویس',
 			'result'   => 'نتیجه وبسرویس',
-		];
+		);
 	}
 
 	public function get_sortable_columns(): array {
-		return [
-			'post_id'  => [ 'post_id', false ],
-			'type'     => [ 'type', false ],
-			'sender'   => [ 'sender', false ],
-			'reciever' => [ 'reciever', false ],
-			'date'     => [ 'date', false ],
-		];
+		return array(
+			'post_id'  => array( 'post_id', false ),
+			'type'     => array( 'type', false ),
+			'sender'   => array( 'sender', false ),
+			'reciever' => array( 'reciever', false ),
+			'date'     => array( 'date', false ),
+		);
 	}
 
 	public function process_bulk_action() {
-
 		$action = $this->current_action();
 
 		if ( 'delete' === $action ) {
@@ -249,14 +250,13 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 			$this->delete_item( intval( $_REQUEST['item'] ?? 0 ) );
 
 			echo '<div class="updated notice is-dismissible below-h2"><p>آیتم حذف شد.</p></div>';
-
-		} else if ( $action == 'bulk_delete' ) {
+		} elseif ( $action == 'bulk_delete' ) {
 
 			if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'] ?? null, 'bulk-' . $this->_args['plural'] ) ) {
 				wp_die( 'خطایی رخ داده است. بعدا تلاش کنید.' );
 			}
 
-			$delete_ids = array_map( 'intval', $_REQUEST['item'] ?? [] );
+			$delete_ids = array_map( 'intval', $_REQUEST['item'] ?? array() );
 
 			foreach ( (array) $delete_ids as $id ) {
 				$this->delete_item( $id );
@@ -269,8 +269,7 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 	public function delete_item( int $id ) {
 		global $wpdb;
 
-		$wpdb->delete( self::table(), [ 'id' => $id ] );
-		
+		$wpdb->delete( self::table(), array( 'id' => $id ) );
 	}
 
 	/*--------------------------------------------*/
@@ -294,7 +293,7 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 	private function table_exists() {
 		global $wpdb;
 
-		return intval( $wpdb->get_var( $wpdb->prepare( "SHOW TABLES LIKE %s" , self::table() ) ) );
+		return intval( $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', self::table() ) ) );
 	}
 
 	private function get_query( $count = false ) {
@@ -302,16 +301,16 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 
 		$select = $count ? 'count(*)' : '*';
 
-		$sql = $wpdb->prepare( "SELECT %s FROM %s", $select, self::table() );
+		$sql = $wpdb->prepare( 'SELECT %s FROM %s', $select, self::table() );
 
 		if ( isset( $_REQUEST['s'] ) ) {
-			$s   = ltrim( sanitize_text_field( $_REQUEST['s'] ), '0' );
-			$sql .= $wpdb->prepare( "WHERE (`message` LIKE %s OR `reciever` LIKE %s  OR `sender` LIKE %s)", '%' . $wpdb->esc_like($s) . '%', '%' . $wpdb->esc_like($s) . '%', '%' . $wpdb->esc_like($s) . '%' );
+			$s    = ltrim( sanitize_text_field( $_REQUEST['s'] ), '0' );
+			$sql .= $wpdb->prepare( 'WHERE (`message` LIKE %s OR `reciever` LIKE %s  OR `sender` LIKE %s)', '%' . $wpdb->esc_like( $s ) . '%', '%' . $wpdb->esc_like( $s ) . '%', '%' . $wpdb->esc_like( $s ) . '%' );
 		}
 
 		if ( ! empty( $_REQUEST['id'] ) ) {
 			$post_id = array_map( 'intval', is_array( $_REQUEST['id'] ) ? $_REQUEST['id'] : explode( ',', (string) $_REQUEST['id'] ) );
-			$sql     .= ( isset( $s ) ? ' AND' : ' WHERE' ) . ' (`post_id` IN (' . implode( ',', sanitize_text_field($post_id) ) . '))';
+			$sql    .= ( isset( $s ) ? ' AND' : ' WHERE' ) . ' (`post_id` IN (' . implode( ',', sanitize_text_field( $post_id ) ) . '))';
 		}
 
 		if ( ! empty( $_REQUEST['orderby'] ) ) {
@@ -328,29 +327,29 @@ class WoocommerceIR_SMS_Archive_List_Table extends WP_List_Table {
 		global $wpdb;
 
 		if ( ! $this->table_exists() ) {
-			return [];
+			return array();
 		}
 
 		$query = $this->get_query();
-		//$query .= $wpdb->prepare("LIMIT %d" , $per_page);
-		//$query .= $wpdb->prepare(" OFFSET ( %d - 1 ) * %d" , $page_number , $per_page);
+		// $query .= $wpdb->prepare("LIMIT %d" , $per_page);
+		// $query .= $wpdb->prepare(" OFFSET ( %d - 1 ) * %d" , $page_number , $per_page);
 
 		return $wpdb->get_results( $query, 'ARRAY_A' );
 	}
 
 	public function get_bulk_actions(): array {
-		return [
+		return array(
 			'bulk_delete' => 'حذف',
-		];
+		);
 	}
-
 }
 
 class WoocommerceIR_SMS_Archive {
 
+
 	public function __construct() {
-		add_action( 'pwoosms_settings_form_bottom_sms_archive', [ $this, 'archiveTable' ] );
-		add_action( 'init', [ $this, 'createTable' ] );
+		add_action( 'pwoosms_settings_form_bottom_sms_archive', array( $this, 'archiveTable' ) );
+		add_action( 'init', array( $this, 'createTable' ) );
 	}
 
 	public static function insertRecord( $data ) {
@@ -362,15 +361,19 @@ class WoocommerceIR_SMS_Archive {
 			$time += wc_timezone_offset();
 		}
 
-		$wpdb->insert( WoocommerceIR_SMS_Archive_List_Table::table(), [
-			'post_id'  => ! empty( $data['post_id'] ) ? $data['post_id'] : 0,
-			'type'     => ! empty( $data['type'] ) ? $data['type'] : 0,
-			'reciever' => ! empty( $data['reciever'] ) ? $data['reciever'] : '',
-			'message'  => ! empty( $data['message'] ) ? $data['message'] : '',
-			'sender'   => ! empty( $data['sender'] ) ? $data['sender'] : '',
-			'result'   => ! empty( $data['result'] ) ? $data['result'] : '',
-			'date'     => gmdate( 'Y-m-d H:i:s', $time ),
-		], [ '%d', '%d', '%s', '%s', '%s', '%s', '%s' ] );
+		$wpdb->insert(
+			WoocommerceIR_SMS_Archive_List_Table::table(),
+			array(
+				'post_id'  => ! empty( $data['post_id'] ) ? $data['post_id'] : 0,
+				'type'     => ! empty( $data['type'] ) ? $data['type'] : 0,
+				'reciever' => ! empty( $data['reciever'] ) ? $data['reciever'] : '',
+				'message'  => ! empty( $data['message'] ) ? $data['message'] : '',
+				'sender'   => ! empty( $data['sender'] ) ? $data['sender'] : '',
+				'result'   => ! empty( $data['result'] ) ? $data['result'] : '',
+				'date'     => gmdate( 'Y-m-d H:i:s', $time ),
+			),
+			array( '%d', '%d', '%s', '%s', '%s', '%s', '%s' )
+		);
 	}
 
 	public function createTable() {
@@ -381,7 +384,7 @@ class WoocommerceIR_SMS_Archive {
 		}
 
 		if ( ! function_exists( 'dbDelta' ) ) {
-			require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+			require_once ABSPATH . '/wp-admin/includes/upgrade.php';
 		}
 
 		$charset_collate = '';
@@ -396,17 +399,19 @@ class WoocommerceIR_SMS_Archive {
 
 		$table = WoocommerceIR_SMS_Archive_List_Table::table();
 
-		dbDelta( "CREATE TABLE IF NOT EXISTS $table (
-			id mediumint(8) unsigned NOT NULL auto_increment,
-			post_id mediumint(8) unsigned,
+		dbDelta(
+			"CREATE TABLE IF NOT EXISTS $table (
+      id mediumint(8) unsigned NOT NULL auto_increment,
+      post_id mediumint(8) unsigned,
             type tinyint(2),
-			reciever TEXT NOT NULL,
-			message TEXT NOT NULL,
-			sender VARCHAR(100),
-			result TEXT,
-			date DATETIME,
-			PRIMARY KEY  (id)
-		) $charset_collate;" );
+      reciever TEXT NOT NULL,
+      message TEXT NOT NULL,
+      sender VARCHAR(100),
+      result TEXT,
+      date DATETIME,
+      PRIMARY KEY  (id)
+    ) $charset_collate;"
+		);
 
 		update_option( 'pwoosms_table_archive', '1' );
 	}
@@ -416,44 +421,43 @@ class WoocommerceIR_SMS_Archive {
 		$list->prepare_items();
 		?>
 
-        <span class="description">پیامک هایی که بعد از نصب نسخه ۴ افزونه ارسال شدند، اینجا نمایش داده خواهند شد.</span>
+  <span class="description">پیامک هایی که بعد از نصب نسخه ۴ افزونه ارسال شدند، اینجا نمایش داده خواهند شد.</span>
 
-        <style type="text/css">
-            .wp-list-table .column-id {
-                max-width: 5%;
-            }
-        </style>
+  <style type="text/css">
+  .wp-list-table .column-id {
+  max-width: 5%;
+  }
+  </style>
 
 		<?php if ( ! empty( $_GET['id'] ) ) : ?>
-            <a class="page-title-action" href="<?php echo remove_query_arg( [ 'id' ] ); ?>">بازگشت به لیست آرشیو
-                همه پیامک ها</a>
-		<?php endif; ?>
+  <a class="page-title-action" href="<?php echo remove_query_arg( array( 'id' ) ); ?>">بازگشت به لیست آرشیو
+  همه پیامک ها</a>
+  <?php endif; ?>
 
-        <form method="post">
-            <input type="hidden" name="page" value="WoocommerceIR_SMS_Archive_list_table">
-			<?php
-			$list->search_box( 'جستجوی گیرنده', 'search_id' );
-			$list->display();
-			?>
-        </form>
+  <form method="post">
+  <input type="hidden" name="page" value="WoocommerceIR_SMS_Archive_list_table">
+		<?php
+		$list->search_box( 'جستجوی گیرنده', 'search_id' );
+		$list->display();
+		?>
+  </form>
 
-        <script type="text/javascript">
-            jQuery(document).ready(function ($) {
-                $('.delete a, a.delete, .button.action').on('click', function (e) {
-                    var action1 = $('select[name="action"]').val();
-                    var action2 = $('select[name="action2"]').val();
-                    if ($(this).is('a') || action1 === 'bulk_delete' || action2 === 'bulk_delete') {
-                        if (!confirm('آیا از انجام عملیات حذف مطمئن هستید؟ این عمل غیرقابل برگشت است.')) {
-                            e.preventDefault();
-                            return false;
-                        }
-                    }
-                });
-            });
-        </script>
+  <script type="text/javascript">
+  jQuery(document).ready(function($) {
+  $('.delete a, a.delete, .button.action').on('click', function(e) {
+	var action1 = $('select[name="action"]').val();
+	var action2 = $('select[name="action2"]').val();
+	if ($(this).is('a') || action1 === 'bulk_delete' || action2 === 'bulk_delete') {
+	if (!confirm('آیا از انجام عملیات حذف مطمئن هستید؟ این عمل غیرقابل برگشت است.')) {
+	e.preventDefault();
+	return false;
+	}
+	}
+  });
+  });
+  </script>
 		<?php
 	}
-
 }
 
 new WoocommerceIR_SMS_Archive();
